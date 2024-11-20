36ed7b818ba52215143bf3e311254f851f86eba5
apoorvagit123 <apoorva2110445@ssn.edu.in>
2024-11-20 13:01:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
-class Main
-{
-    public static void main(String []args)
-    {
-        System.out.println("Hello World!!");
+// class Main
+// {
+//     public static void main(String []args)
+//     {
+//         System.out.println("Hello World!!");
+//     }
+// };
+
+public class StaticArithmeticOperations {
+    public static void main(String[] args) {
+        // Hardcoded numbers
+        int num1 = 20;
+        int num2 = 10;
+
+        // Perform arithmetic operations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Display the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
     }
-};
+}
